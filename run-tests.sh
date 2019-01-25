0c185afeaf6ebf57c3e55dde9dbc12ec4c467018
Robert Kolmos <robertk3@cs.washington.edu>
2019-01-25 09:19:48 -0800

    add run tests script

---
+++ run-tests.sh
@@ -0,0 +1,7 @@
+cd /tmp
+git clone https://github.com/Robert-Kolmos/java.git jsoniter
+cd jsoniter
+git remote show origin | grep "Fetch URL:" > test-execution.txt
+echo SHA: ($git rev-parse HEAD) >> test-execution.txt
+time mvn test >> test-execution.txt
+
