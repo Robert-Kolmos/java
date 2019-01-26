684f094c9f56702b657cf35b30223074de41d974
Robert Kolmos <robertk3@cs.washington.edu>
2019-01-25 21:52:12 -0800

    remove turnin files from repo

--- run-coverage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /tmp
-git clone https://github.com/Robert-Kolmos/java.git jsoniter
-cd jsoniter/
-mvn test > /dev/null
-mvn org.jacoco:jacoco-maven-plugin:report
-mv target/sites/jacoco/index.html test-coverage.html
-
--- run-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /tmp
-git clone https://github.com/Robert-Kolmos/java.git jsoniter
-cd jsoniter
-git remote show origin | grep "Fetch URL:" > test-execution.txt
-echo "SHA: ($git rev-parse HEAD)" >> test-execution.txt
-time mvn test >> test-execution.txt
-
